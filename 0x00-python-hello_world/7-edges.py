5d654ff37e4987f184c7c68367b3d008ba3d08c3
Xmuluneh <mulabr673@gmail.com>
2022-05-30 18:15:07 +0300

    Complete task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+a = word[:3]
+b = word[-2:]
+c = word[1:len(word)-1]
+print(f"First 3 letters: {a}".format(a))
+print(f"Last 2 letters: {b}".format(b))
+print(f"Middle word: {c}".format(c))
