47a60e5278181a4134ba1e5683c674804df78026
Xmuluneh <mulabr673@gmail.com>
2022-06-08 19:14:52 +0300

    Complete task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """Return the square of alist without for loop"""
-    return list(map(lambda x: x * number, my_list))
+    """Return the square of a list without for loop"""
+    return (list(map(lambda x: x * number, my_list)))
