777c44d18d863688f967402433da49fce3793cdb
Xmuluneh <mulabr673@gmail.com>
2022-06-08 16:31:09 +0300

    Complete task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+def uniq_add(my_list=[]):
+    """add the unique elements in the list"""
+    new_set = set(my_list)
+    element_sum = 0
+    for i in set:
+        sum += i
+    return element_sum
