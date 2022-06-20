c70e8da51d2954b802207ab334965896b0750b94
Xmuluneh <mulabr673@gmail.com>
2022-06-20 13:40:16 +0300

    Complete task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print('{:d}'.format(i))
             element_count += 1
         except IndexError:
-            print("Sorry invalid index")
+            pass
     print()
     return element_count
