86d77aa7c208aea6502c722106caa2d42854011f
Xmuluneh <mulabr673@gmail.com>
2022-06-20 14:03:26 +0300

    Complete task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print('{:d}'.format(i))
             element_count += 1
-        except:
-            pass
-    print()
+        except IndexError:
+            break
+    print("")
     return element_count
