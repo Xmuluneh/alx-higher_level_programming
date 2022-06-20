d14be62413dd088953ad00f8efb968be10738807
Xmuluneh <mulabr673@gmail.com>
2022-06-20 21:59:52 +0300

    main file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     """The function do prnt integer values and retuen True """
     try:
         print("{:d}".format(value))
         num = True
     except:
-        print("Exception:",sys.exc_info()[1],file=sys.stderr)
+        print("Exception:", sys.exc_info()[1], file=sys.stderr)
         num = False
     return num
