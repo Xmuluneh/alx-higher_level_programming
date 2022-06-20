37df880c6ca72edf0bbc40c15dc9f593336bf437
Xmuluneh <mulabr673@gmail.com>
2022-06-20 16:42:37 +0300

    Complete task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,8 +3,8 @@ import sys
 def safe_print_integer_err(value):
     """The function do prnt integer values and retuen True """
     try:
-        print("{:d}".format(value))
+        print("{:d} ".format(value))
         return True
     except(TypeError,ValueError):
-        print("Exception:{}".format(sys.exc_info()[1],file=sys.stderr))
+        print("Exception:{} ".format(sys.exc_info()[1],file=sys.stderr))
         return False
