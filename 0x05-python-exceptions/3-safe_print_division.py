3e29540fbb3b6702d53dc95b6dc8b8fbc9a85143
Xmuluneh <mulabr673@gmail.com>
2022-06-20 15:29:04 +0300

    Complete task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         num = None
     finally:
-        print("{}".format(num))
+        print("Inside result:{}".format(num))
     return num
