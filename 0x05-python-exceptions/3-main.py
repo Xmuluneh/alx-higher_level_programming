9c6e018bbc9a1dcb37c4851023f6c8301c7a3856
Xmuluneh <mulabr673@gmail.com>
2022-06-20 15:39:28 +0300

    Complete task 3

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -2,7 +2,7 @@
 safe_print_division = __import__('3-safe_print_division').safe_print_division
 
 a = 0
-b = 2
+b = 0
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
 
