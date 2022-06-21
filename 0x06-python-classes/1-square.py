d151ddd9bdee3698731f1cefa23cde7cbd4a80fe
Xmuluneh <mulabr673@gmail.com>
2022-06-21 14:53:08 +0300

    Complete task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class square is defines based on the 0-Square"""
+
+
+class Square:
+    """A Square class with a private attribute"""
+
+    def __init__(self, size):
+        self.__size = size
