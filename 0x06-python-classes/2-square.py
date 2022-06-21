75e91defd7d9330c8afac64c8e4dec8e473e39ec
Xmuluneh <mulabr673@gmail.com>
2022-06-21 15:35:17 +0300

    Complete tak 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class square is defined based on 1-square.py"""
+
+
+class Square:
+    """ A square class with private attribute size"""
+    def __int__(self, size=0):
+        """Initialize the size variable"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        else:
+            raise ValueError("size must be >=0")
