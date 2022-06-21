0fde6ceae85c12517123c63b4bbd733283ba3453
Xmuluneh <mulabr673@gmail.com>
2022-06-21 15:47:31 +0300

    Complete task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,15 @@
 
 class Square:
     """ A square class with private attribute size"""
+
     def __int__(self, size=0):
         """Initialize the size variable"""
         self.__size = size
+
+    def set_size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
