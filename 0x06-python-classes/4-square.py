e82e409c565ee91b9c093ac29bd793f07e53d8b2
Xmuluneh <mulabr673@gmail.com>
2022-06-22 11:39:57 +0300

    Complete task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python3
-"""The class is defined based on 3-square.py"""
+"""a class Square that defines a square
+by: (based on 3-square.py)
+
+"""
 
 
 class Square:
+    """Square class with a private attribute -
+    size.
+
+    """
+
     def __init__(self, size=0):
-        """Initialize the size value
-        Args:
-            size: the size of a square is initialized
-            """
+        """Initializes the size variable as a private
+        instance artribute
+
+        """
         self.__size = size
 
     @property
     def size(self):
-        """Return the optimal value of size
-        Args:
-            size : The size of a square
-            """
+        """Instantiation with optional size of square"""
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """Set a new value for size if the conditions are passed.
-         Args:
-             size:set a new value for size
-             """
-        if not isinstance(value, int):
+    def size(self, size_value):
+        """Gets the size of the square"""
+        self.__size = size_value
+
+        if not isinstance(size_value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size_value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
 
     def area(self):
-        """Calculate the area of the square and return value """
+        """Returns the current square area"""
         return self.__size ** 2
