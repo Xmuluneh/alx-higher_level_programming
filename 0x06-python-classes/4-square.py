65ae2a658569ce9f067811147f98a01475961e5c
Xmuluneh <mulabr673@gmail.com>
2022-06-22 11:23:27 +0300

    Complete task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/use/bin/python3
+"""The class is defined based on 3-square.py"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """Initialize the size value"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Return the optimal value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set a new value for size if the conditions are passed. """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculate the area of the square"""
+        return self.__size ** 2
