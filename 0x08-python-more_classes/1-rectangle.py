f9e55cb97a8df2f66da8ababe0730e2bf8cdc069
Xmuluneh <mulabr673@gmail.com>
2022-06-27 16:56:02 +0300

    Bug fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
              width(int) with a new width
              height(int) with a new height
              """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
