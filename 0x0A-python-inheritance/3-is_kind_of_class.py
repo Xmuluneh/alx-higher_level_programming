62b9168e5337c7a1b7907e9db14d7805bdd72214
Xmuluneh <mulabr673@gmail.com>
2022-07-04 13:59:18 +0300

    Complete task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""3-is_kind_of_class check the instance of the object to the given call or inheres """
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    My function is_kind_of_class:
+    Args:
+        first parameter:obj
+        second parameter: a_class
+    Return:
+        a boolean value after check is instantiation
+    """
+    return True if type(obj) == a_class else False
+
