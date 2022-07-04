eb04e26ba20211c4463694eaf2caafa0e47ab150
Xmuluneh <mulabr673@gmail.com>
2022-07-04 16:04:33 +0300

    Bug fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Only subclass of """
+"""Module 4-inherits_from.
+Finds if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class."""
 
 
 def inherits_from(obj, a_class):
-    """My function inherits_from:checks instance of the object from the inherited directly or indirect
-      Args:
-          first parameter: obj
-          second parameter a_class
-      Return :
-              a bool value
-     """
-    if isinstance(type(obj), a_class) and type(obj) != a_class:
-        return True
-    else:
-        return False
+    """Determines if obj is an instance of a class that
+    inherited from a_class.
+    Args:
+        - obj: object to look at
+        - a_class: class to evaluate
+    Returns: True or False
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
