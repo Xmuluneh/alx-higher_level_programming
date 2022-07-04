fe5a7ad4926d842d494752cb3dedc1ec1bfbbea2
Xmuluneh <mulabr673@gmail.com>
2022-07-04 14:40:37 +0300

    Complete task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class bsed on 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """A class BaseGeometry"""
+    def area(self):
+        """My function area : with no parameter"""
+        raise Exception("area() is not implemented")
