4d4f4eeda8ee05e708c0a8f393c75546a08d4287
Xmuluneh <mulabr673@gmail.com>
2022-07-05 14:12:05 +0300

    Complete task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Module 2-append_write.
-   Append in the file text
+   Append a string at the end of a  text file
 """
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a text file
+    """Appends a text file
         Args:
             -filename:name of the file
             -text:string append in the file
         Return :number of character append
 
     """
-    with open(filename, ' a+') as f:
+    with open(filename, 'a+') as f:
         return f.write(text)
