52b6b83f68b1320e713518c991da93a9d303b701
Xmuluneh <mulabr673@gmail.com>
2022-07-05 12:03:40 +0300

    Complete task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,6 @@ def read_file(filename=""):
 
      """
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+        read_text = f.read()
+        print(read_text, end="")
     f.close()
