9553bd2947e050d914904df666b496ea20d1805c
Xmuluneh <mulabr673@gmail.com>
2022-07-05 13:58:14 +0300

    Complete task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module 1-write_file
+    Write in the text file
+"""
+
+
+def write_file(filename="", text=""):
+    """ Write text in  a file
+        Args:
+            -filename: name of the file
+            -text : string to write i the file
+         Return :number of character written
+
+
+    """
+    with open(filename, 'w+') as f:
+        return f.write(text)
