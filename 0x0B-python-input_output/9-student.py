40d44c09694567a8e1065e37226a426d1a730a8a
Xmuluneh <mulabr673@gmail.com>
2022-07-05 16:43:29 +0300

    Completet task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,8 +1,23 @@
+#!/usr/bin/python3
 class Student:
+    """ Class:Student
+
+    """
+
     def __init__(self, first_name, last_name, age):
+        """initialise the parameter list
+            Args:
+                -first par.: first_name
+                - Second par.:last_name
+                -third par.:age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def  to_json(self):
+    def to_json(self):
+        """the functon to_json return the dictionary
+        representation of the clas Strident
+        """
 
+        return self.__dict__
