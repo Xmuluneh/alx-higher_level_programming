918ca518d7e273efa9424a4c444c88d9920197f5
Xmuluneh <mulabr673@gmail.com>
2022-07-05 12:02:37 +0300

    Complete task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module 0-read_file.
+Read from a file and print
+"""
+
+
+def read_file(filename=""):
+    """MY function read_file: read text from file and print its content on the stdout
+    Args:
+        first parameter: filename string
+
+     """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
+    f.close()
