a7a28cbc0ad68cc40fc01cbfc916d0a295792169
Xmuluneh <mulabr673@gmail.com>
2022-08-09 15:36:55 +0300

    Create table with values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
--- A script the creat second table 
+-- A script the creat second_table in the db
+-- attribute id,name and score
 CREAT TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
 INSERT INTO second_table(id,name,score)VALUES (1,"John",10),(2,"Alex",3),(3,"Bob",14),(4,"George",8);
