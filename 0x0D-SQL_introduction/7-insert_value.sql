7f58f208443216f8a54e062c8a6948906288eb44
Xmuluneh <mulabr673@gmail.com>
2022-08-09 15:22:21 +0300

    Insert value in to a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- A script that insert new row in the table
-INSERT INTO first_name (id, name) VALUES (89, "Best School")
+-- A script that insert new row in the table first_table (Db hbtn_0c_0)
+INSERT INTO first_table (id, name) VALUES (89, "Best School")
