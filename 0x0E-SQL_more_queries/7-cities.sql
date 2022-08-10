dfd9c6231da7173b49da7c3c633812edbe720cd8
Xmuluneh <mulabr673@gmail.com>
2022-08-10 15:37:41 +0300

    Create DB & table witt PK,FK

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
 -- A script that creates the database hbtn_0d_usa and
 -- the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states, name VARCHAR(256) NOT NULL);
