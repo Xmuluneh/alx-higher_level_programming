2422d006b05558fef246ad0938e636ed850a1e4a
Xmuluneh <mulabr673@gmail.com>
2022-08-10 22:57:12 +0300

    Complete task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- A script that lists all shows contained in the database hbtn_0d_tvshows
 SELECT ts.title, tsg.genre_id 
 FROM tv_shows ts
-JOIN tv_show_genres tsg 
+LEFT JOIN tv_show_genres tsg 
 ON ts.id = tsg.show_id 
 ORDER BY ts.title, tsg.genre_id
