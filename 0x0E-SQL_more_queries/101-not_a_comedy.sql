a6a3296c7d2efdf35aded64bc6957e7f5357b2ee
Xmuluneh <mulabr673@gmail.com>
2022-08-10 22:25:44 +0300

    Complete task 18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 --  A script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 SELECT title
 FROM tv_shows 
-WHEREtitle NOT IN
+WHERE title NOT IN
 	(SELECT TITLE
 		FROM tv_shows ts
 		JOIN tv_show_genres tsg
