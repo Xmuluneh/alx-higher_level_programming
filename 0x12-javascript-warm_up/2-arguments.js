0f88efe39de8da276391abf2d739011a8b5f8e6f
Xmuluneh <mulabr673@gmail.com>
2022-09-05 23:15:36 +0300

    Process argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argLen = process.argv.length;
 if (argLen === 3) {
-  console.log("Argument Found");
+  console.log('Argument Found');
 } else if (argLen > 3) {
-  console.log("Arguments found");
+  console.log('Arguments found');
 } else {
-  console.log("No argument");
+  console.log('No argument');
 }
