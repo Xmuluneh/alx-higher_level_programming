4c0f90d8c89925bbba37e8374a89f783ea3bd311
Xmuluneh <mulabr673@gmail.com>
2022-09-06 16:31:46 +0300

    Bug fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
-#!/usr/bin/node
+#!/opt/homebrew/bin/node
 const argLen = process.argv.length;
 if (argLen === 3) {
-  console.log('Argument Found');
+  console.log('Argument found');
 } else if (argLen > 3) {
   console.log('Arguments found');
 } else {
