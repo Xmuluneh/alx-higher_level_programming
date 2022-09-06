8204674ca0a275e086a0a49b123262037b802e81
Xmuluneh <mulabr673@gmail.com>
2022-09-06 11:53:07 +0300

    Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/opt/homebrew/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w === undefined || h === undefined || w <= 0 || h <= 0) {
+      module.exports = Rectangle;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
