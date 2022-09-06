3078d0e1f38ff790ffe182c0e44c7db8b5166f6f
Xmuluneh <mulabr673@gmail.com>
2022-09-06 15:32:47 +0300

    File concat

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,7 @@
+#!/opt/homebrew/bin/node
+import { dict } from './101-data';
+const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
+  acc[value] = acc[value] ? [...acc[value], key] : [key];
+  return acc;
+}, {});
+console.log(myValue);
