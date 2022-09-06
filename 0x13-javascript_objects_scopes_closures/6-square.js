4d82be17534d351622a494ee1c61ffbfecf9770c
Xmuluneh <mulabr673@gmail.com>
2022-09-06 13:57:58 +0300

    Square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,33 @@
+#!/opt/homebrew/bin/node
+const Rectangle = require('./5-square');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let letPrint = '';
+    if (c === undefined) {
+      for (let i = 0; i < this.height; i++) {
+        for (let k = 0; k < this.width; k++) {
+          letPrint += 'X';
+        }
+        if (i !== this.height - 1) {
+          letPrint += '\n';
+        }
+      }
+      console.log(letPrint);
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let k = 0; k < this.width; k++) {
+          letPrint += c;
+        }
+        if (i !== this.height - 1) {
+          letPrint += '\n';
+        }
+      }
+      console.log(letPrint);
+    }
+  }
+}
+module.exports = Square;
