f1582d52f38aa2638d340b7bc1ff9ad674a44a7d
Xmuluneh <mulabr673@gmail.com>
2022-09-06 12:36:20 +0300

    rectange class with swap fun

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/opt/homebrew/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      module.exports = Rectangle;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let recShape = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let k = 0; k < this.width; k++) {
+        recShape += 'X';
+      }
+      if (i !== this.height - 1) {
+        recShape += '\n';
+      }
+    }
+    console.log(recShape);
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
+}
+module.exports = Rectangle;
