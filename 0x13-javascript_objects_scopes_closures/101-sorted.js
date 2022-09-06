c42de9b21cf8caa65027da125e75ff47df092d23
Xmuluneh <mulabr673@gmail.com>
2022-09-06 21:32:44 +0300

    buf fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-import { dict } from './101-data';
+
+const { dict } = require('./101-data');
+
 const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
   acc[value] = acc[value] ? [...acc[value], key] : [key];
   return acc;
