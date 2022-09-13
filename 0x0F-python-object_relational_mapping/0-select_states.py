e75439e584897044ba2b29465f3e9941c457ff51
Xmuluneh <mulabr673@gmail.com>
2022-09-13 21:52:46 +0300

    Complete task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,16 +3,17 @@
 (upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
-
     import MySQLdb
-    import sys
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # import sys
 
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC""")
-    rows = cur.fetchall()
-    for row in rows:
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user="root", passwd="passwd123",
+                           db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
+    cur.close()
+    conn.close()
