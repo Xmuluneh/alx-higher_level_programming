87e0ddddcf3791be096d7aa1aff2b75e7f1b3efe
Xmuluneh <mulabr673@gmail.com>
2022-09-13 22:26:52 +0300

    Task completed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,12 @@ if __name__ == '__main__':
 
     import MySQLdb
     import sys
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user="root", passwd="passwd123", db=database)
 
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
