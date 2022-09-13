c91bf737a71fb960e5b81be1e266f8d3f203d185
Xmuluneh <mulabr673@gmail.com>
2022-09-13 22:16:50 +0300

    Complete task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N
-(upper N) from the database hbtn_0e_0_usa"""
+"""lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
     import MySQLdb
