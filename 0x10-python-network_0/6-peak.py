311d0a5369065cb4dc31563d9a49f755cfc2b4d8
Xmuluneh <mulabr673@gmail.com>
2022-09-22 14:53:08 +0300

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,9 @@ def find_peak(list_of_integers):
     """Find the peck element in the list"""
     if len(list_of_integers)==1:
         return list_of_integers[0]
+    for i in range(len(list_e)-1):
+        if list_e[i] < list_e[i + 1]:
+            num = list_e[len(list_e) - 1]
     if list_of_integers is None or list_of_integers == []:
         return None
     else:
