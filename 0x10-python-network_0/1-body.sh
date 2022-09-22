8c75d51e24b9963b0806ce3433e3d414a6b70507
Xmuluneh <mulabr673@gmail.com>
2022-09-22 12:31:23 +0300

    Bug fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1"  -L 
+# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -s "$1" -L
