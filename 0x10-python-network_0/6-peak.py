169fb182fb6c662fef9bd57c45077c3198c09c84
Xmuluneh <mulabr673@gmail.com>
2022-09-22 13:06:57 +0300

    Peack task

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Find the maximum element in the list """
+
+
+def find_peak(list_of_integers):
+    """Find the peck element in the list"""
+    if len(list_of_integers)==1:
+        return list_of_integers[0]
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    else:
+        num = 0
+        for i in list_of_integers:
+            if i > num:
+                num = i
+        return num
