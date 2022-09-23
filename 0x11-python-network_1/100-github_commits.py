95f9129a4a07ff39dc90229b8adb19b67b960617
Xmuluneh <mulabr673@gmail.com>
2022-09-23 16:55:14 +0300

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     if rul.status_code >= 400:
         print('None')
     else:
-        for commit in rul.json()[:10]:
+        for commit in rul.json[:10]:
             print('{}: {}'.format(commit.get('sha'), commit.get('commit').
                                   get('author').get('name')))
