270186072e15a8adddbf595be12b6d0e8806ea39
Xmuluneh <mulabr673@gmail.com>
2022-09-23 13:36:41 +0300

    Task completed

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""Takes in a URL, sends a request to the URL and
+displays the body of the response
+"""
+if __name__ == '__main__':
+    import requests as r
+    import sys
+    rul = r.request.get(sys.argv[1])
+    if rul.status_code >= 400:
+        print("Error code: {}".format(rul.status_code))
+    else:
+        print(rul.text)
