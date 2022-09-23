40b5e4f1e4058914e2c1b0b3c3f74bd53b9e9071
Xmuluneh <mulabr673@gmail.com>
2022-09-23 16:44:28 +0300

    fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ displays the body of the response
 if __name__ == '__main__':
     import requests as r
     import sys
-    rul = r.request.get(sys.argv[1])
+    rul = r.get(sys.argv[1])
     if rul.status_code >= 400:
         print("Error code: {}".format(rul.status_code))
     else:
