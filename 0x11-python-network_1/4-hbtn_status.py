f045c48a5f689177c9cb394a673f84d4caebf64b
Xmuluneh <mulabr673@gmail.com>
2022-09-23 16:37:31 +0300

    fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     import requests as r
     rul = r.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type:{}'.format(type(rul.text)))
+    print('\t- type: {}'.format(type(rul.text)))
     print('\t- content: {}'.format(rul.text))
