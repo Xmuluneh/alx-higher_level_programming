ecf84013a72504981a0d0ece3e85785f59027eae
Xmuluneh <mulabr673@gmail.com>
2022-09-23 16:57:34 +0300

    bug fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """Takes in Github repo nd owner name to list
 10 commits (from the most recent to oldest)"""
-if __name__ == '__main__':
-    import requests as r
+
+if __name__ == "__main__":
+    import requests
     import sys
 
-    rul = r.get('https:api.github.com/repos/{}/{}/commits'.
-                format(sys.argv[2], sys.argv[1]))
-    if rul.status_code >= 400:
-        print('None')
-    else:
-        for commit in rul.json[:10]:
-            print('{}: {}'.format(commit.get('sha'), commit.get('commit').
-                                  get('author').get('name')))
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    response = requests.get(
+            "https://api.github.com/repos/{}/{}/commits"
+            .format(owner, repo))
+
+    auth_json = response.json()
+
+    for comm in auth_json[:10]:
+        print("{}: {}".format(
+            comm.get("sha"),
+            comm.get("commit")
+            .get("author")
+            .get("name")))
