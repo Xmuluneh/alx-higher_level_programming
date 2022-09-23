898c2c63a026eebfc21089c8165dcecc6b29a4ec
Xmuluneh <mulabr673@gmail.com>
2022-09-23 14:15:59 +0300

    Task completed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-""""""
+"""Takes in Github credentials (username and password) and uses the Github API
+to display an id
+"""
 if __name__ == '__main__':
     import requests
     import sys
-
+    rul = requests.get('https://api.github.com/user',
+                       auth=(sys.argv[1], sys.argv[2]))
+    if rul.status_code >= 400:
+        print('None')
+    else:
+        print(rul.json().get('id'))
