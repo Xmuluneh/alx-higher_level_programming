d290ecd08c388956bdbc7233eea2480454c12260
Xmuluneh <mulabr673@gmail.com>
2022-09-23 14:47:56 +0300

    Task completed

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes in Github repo nd owner name to list
+10 commits (from the most recent to oldest)"""
+if __name__ == '__main__':
+    import requests as r
+    import sys
+    rul = r.get('https:api.github.com/repos/{}/{}/commits'.
+                format(sys.argv[2], sys.argv[1]))
+    if rul.status_code >= 400:
+        print('None')
+    else:
+        for commit in rul.json()[10]:
+            print('{}: {}'.fomat(commit.get('sha'), commit.get('commit').
+                                 get('author').get('name')))
